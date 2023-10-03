17fb6bf21ccb77fc4042b23ee291ab972b05698b
mlwelker <nutria.watts.0g@icloud.com>
2023-10-02 22:49:46 -0500

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Michael Welker on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
